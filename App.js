747206452fb22c2459d1b814a817d02c8909c674
EnzoKurt <142853969+EnzoKurt@users.noreply.github.com>
2025-08-13 00:54:38 +0000

    versao inicial

---
+++ App.js
@@ -0,0 +1,19 @@
+import {View, Text,StyleSheet} from "react-native"
+
+const App = () => {
+  return(
+    <View style={styles.content}>
+       <Text>Hello World!</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  content:{
+    flex:1,
+    justifyContent: "center",
+alignItems:"center",
+backgroundColor:"gray"
+  }
+})
+
+export default App;
